8abf11bf6645abb6d399b44b0461352b7185cc08
Caelum20 <caelum340@gmail.com>
2025-03-29 18:49:50 -0600

    yep

--- src/main/java/net/caelum/poolmod/block/ModBlocks.java
+++ src/main/java/net/caelum/poolmod/block/ModBlocks.java
@@ -5,6 +5,8 @@ import net.caelum.poolmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GlowLichenBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
@@ -19,10 +21,10 @@ public class ModBlocks {
 
     public static final DeferredBlock<Block> POOLTILE_BLOCK = registerBlock("pooltile_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops()));
-    public static final DeferredBlock<Block> LIMINAGLAE_BLOCK = registerBlock("liminalgae_block",
+                    .strength(-1f).sound(SoundType.DEEPSLATE_TILES)));
+    public static final DeferredBlock<Block> LIMINAGLAE_BLOCK = registerBlock("liminalgae_block.json",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(1f).sound(SoundType.GRASS).lightLevel(state -> 10)));
+                    .strength(.2f).sound(SoundType.GRASS).lightLevel(GlowLichenBlock.emission(7)).instabreak().noOcclusion().randomTicks()));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
